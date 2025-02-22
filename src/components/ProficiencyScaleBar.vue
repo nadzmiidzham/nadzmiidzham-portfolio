ccbb31397b5fd3e1ab37bb89891b35ad938b7ad0
Nadzmi Idzham <nadzmiidzham@gmail.com>
2025-02-23 03:39:44 +0800

    Added ProficiencyScaleBar.vue & SkillSummarySection.vue

---
+++ src/components/ProficiencyScaleBar.vue
@@ -0,0 +1,26 @@
+<script setup>
+const { label, value } = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+
+  // value range = between 0-10
+  value: {
+    type: Number,
+    default: 0,
+    validator: (v) => v >= 0 && v <= 10,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h1>{{ label }}</h1>
+    <div
+      class="h-3 overflow-hidden rounded-full bg-green-900 text-yellow-600 outline"
+    >
+      <div :class="`h-3 w-${value}/10 rounded-full bg-yellow-500`"></div>
+    </div>
+  </div>
+</template>
--- src/sections/SkillSummarySection.vue
+++ src/sections/SkillSummarySection.vue
@@ -1,3 +1,55 @@
+<script setup lang="ts">
+import ProficiencyScaleBar from "../components/ProficiencyScaleBar.vue";
+
+const programmingLanguageList = [
+  { label: "Javascript / Typescript", value: 9 },
+  { label: "PHP", value: 9 },
+  { label: "Java / Kotlin", value: 8 },
+];
+
+const frameworkList = [
+  { label: "NestJS", value: 9 },
+  { label: "NuxtJS", value: 8 },
+  { label: "Laravel", value: 9 },
+  { label: "Slim Framework", value: 9 },
+  { label: "Flutter", value: 5 },
+  { label: "Android Studio (Java / Kotlin)", value: 6 },
+];
+
+const devopToolList = [
+  { label: "Docker", value: 8 },
+  { label: "Gitlab CI/CD", value: 6 },
+  { label: "ArgoCD", value: 5 },
+];
+</script>
+
 <template>
-  <h1>Skill Summary Section</h1>
+  <div class="py-3">
+    <!-- Programming Language -->
+    <div>
+      <h1><b>Programming Language</b></h1>
+
+      <div v-for="item in programmingLanguageList">
+        <ProficiencyScaleBar :label="item.label" :value="item.value" />
+      </div>
+    </div>
+
+    <!-- framework skill -->
+    <div>
+      <h1><b>Framework</b></h1>
+
+      <div v-for="item in frameworkList">
+        <ProficiencyScaleBar :label="item.label" :value="item.value" />
+      </div>
+    </div>
+
+    <!-- DevOp Tool -->
+    <div>
+      <h1><b>DevOp Tool</b></h1>
+
+      <div v-for="item in devopToolList">
+        <ProficiencyScaleBar :label="item.label" :value="item.value" />
+      </div>
+    </div>
+  </div>
 </template>

2102ddd842b5c6c20bfdfd308f21510f9690c14f
nadzmiidzham <nadzmiidzham@gmail.com>
2025-03-05 19:56:55 +0800

    init detail section

--- src/layouts/DetailSectionLayout.vue
+++ src/layouts/DetailSectionLayout.vue
@@ -1,8 +1,6 @@
 <template>
   <!-- TODO: remove the top most div before deployment -->
   <div class="bg-white">
-    <div>
-      <h1>Detail Section</h1>
-    </div>
+    <h1>Detail Section</h1>
   </div>
 </template>

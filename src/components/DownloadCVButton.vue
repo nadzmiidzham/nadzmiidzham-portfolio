1216019460553d54d688b8f9751fbe9e98482187
nadzmiidzham <nadzmiidzham@gmail.com>
2025-03-06 02:38:25 +0800

    fixed download button color

--- src/components/DownloadCVButton.vue
+++ src/components/DownloadCVButton.vue
@@ -4,8 +4,8 @@
     class="text-white hover:text-yellow-300"
     download
   >
-    <div class="px-3 py-2 rounded-2xl bg-green-950 text-center hover:bg-green-700 hover:outline">
-      <b>Download CV</b>
+    <div class="px-3 py-2 rounded-2xl text-green-950 bg-yellow-500 text-center outline hover:bg-green-900 hover:text-yellow-500">
+      <b>Download My CV</b>
     </div>
   </a>
 </template>

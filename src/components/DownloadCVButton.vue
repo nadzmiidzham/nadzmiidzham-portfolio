98cc6681aef90a3e2964e33a8492db21e288268e
nadzmiidzham <nadzmiidzham@gmail.com>
2025-03-05 16:18:19 +0800

    added burger menu for skill summary

--- src/components/DownloadCVButton.vue
+++ src/components/DownloadCVButton.vue
@@ -1,9 +1,11 @@
 <template>
   <a
     href="/documents/cv-manual-2025.pdf"
-    class="rounded-full bg-green-950 p-2 text-center text-white hover:bg-green-700 hover:text-yellow-300 hover:outline"
+    class="text-white hover:text-yellow-300"
     download
   >
-    <b>Download CV</b>
+    <div class="px-3 py-2 rounded-2xl bg-green-950 text-center hover:bg-green-700 hover:outline">
+      <b>Download CV</b>
+    </div>
   </a>
 </template>
--- src/layouts/SummarySectionLayout.vue
+++ src/layouts/SummarySectionLayout.vue
@@ -1,18 +1,27 @@
-<script setup lang="ts">
+<script setup>
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from '@headlessui/vue'
 import DownloadCVButton from "../components/DownloadCVButton.vue";
 import Avatar from "../components/Avatar.vue";
 import SkillSummarySection from "../sections/SkillSummarySection.vue";
 </script>
 
 <template>
-  <div class="flex flex-col gap-2">
-    <div class="m-auto"><Avatar /></div>
-    <div class="m-auto"><DownloadCVButton /></div>
-    <div class="mx-10"><SkillSummarySection /></div>
+  <div class="flex flex-col gap-2 my-3">
+    <div class="mx-auto"><Avatar /></div>
+    <div class="mx-3"><DownloadCVButton /></div>
+    
+    <Disclosure>
+      <DisclosureButton class="mx-3 rounded-2xl bg-green-950 py-2 px-3 text-center text-white hover:bg-green-700 hover:text-yellow-300 hover:outline">
+        <b>Skill Summary</b>
+      </DisclosureButton>
+
+      <DisclosurePanel>
+        <div class="mx-7"><SkillSummarySection /></div>
+      </DisclosurePanel>
+    </Disclosure>
   </div>
-  <!-- <div class="flex flex-col gap-2">
-    <Avatar />
-    <DownloadCVButton />
-    <SkillSummarySection />
-  </div> -->
 </template>

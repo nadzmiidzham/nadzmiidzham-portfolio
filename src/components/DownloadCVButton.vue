2540226dc639b1f30721317afa4ce929f385137c
nadzmiidzham <nadzmiidzham@gmail.com>
2025-03-17 15:25:29 +0800

    changed color coding for download CV button on desktop

--- src/components/DownloadCVButton.vue
+++ src/components/DownloadCVButton.vue
@@ -1,10 +1,26 @@
 <template>
+  <!-- mobile view -->
   <a
     href="/documents/cv-manual-2025.pdf"
-    class="text-white hover:text-yellow-300"
+    class="text-white hover:text-yellow-300 md:hidden"
     download
   >
-    <div class="px-3 py-2 rounded-2xl text-green-950 bg-yellow-500 text-center outline hover:bg-green-900 hover:text-yellow-500">
+    <div
+      class="rounded-2xl bg-yellow-500 px-3 py-2 text-center text-green-950 outline hover:bg-green-900 hover:text-yellow-500"
+    >
+      <b>Download My CV</b>
+    </div>
+  </a>
+
+  <!-- desktop view -->
+  <a
+    href="/documents/cv-manual-2025.pdf"
+    class="hidden text-white hover:text-yellow-300 md:block"
+    download
+  >
+    <div
+      class="rounded-2xl bg-green-900 px-3 py-2 text-center text-yellow-500 outline hover:bg-yellow-500 hover:text-green-950"
+    >
       <b>Download My CV</b>
     </div>
   </a>

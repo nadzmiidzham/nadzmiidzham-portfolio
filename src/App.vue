e2d1de1a315d8b3592d2a1c51d628d676595d46d
nadzmiidzham <nadzmiidzham@gmail.com>
2025-03-17 10:23:55 +0800

    fixed summary section desktop

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,17 @@ import SummarySectionLayout from "./layouts/SummarySectionLayout.vue";
 </script>
 
 <template>
-  <div class="flex flex-col">
+  <!-- mobile view -->
+  <div class="flex flex-col md:hidden">
     <SummarySectionLayout />
     <DetailSectionLayout />
   </div>
-  <!-- <div class="grid grid-cols-6 justify-items-center gap-4 py-7">
+
+  <!-- desktop view -->
+  <div class="hidden grid-cols-7 justify-items-center gap-4 py-7 md:grid">
     <div class="col-span-1"></div>
     <div class="col-span-3"><DetailSectionLayout /></div>
-    <div class="col-span-1"><SummarySectionLayout /></div>
+    <div class="col-span-2"><SummarySectionLayout /></div>
     <div class="col-span-1"></div>
-  </div> -->
+  </div>
 </template>
--- src/layouts/SummarySectionLayout.vue
+++ src/layouts/SummarySectionLayout.vue
@@ -1,24 +1,27 @@
 <script setup>
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-} from '@headlessui/vue';
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
 import DownloadCVButton from "../components/DownloadCVButton.vue";
 import Avatar from "../components/Avatar.vue";
 import SkillSummarySection from "../sections/SkillSummarySection.vue";
 </script>
 
 <template>
-  <div class="flex flex-col gap-2 my-3">
+  <!-- mobile view -->
+  <div class="my-3 flex flex-col gap-2 md:hidden">
     <div class="mx-auto"><Avatar /></div>
     <div class="mx-3"><DownloadCVButton /></div>
 
     <Disclosure v-slot="{ open }">
-      <DisclosureButton v-if="open" class="mx-3 rounded-2xl bg-yellow-400 py-2 px-3 text-center text-green-950 outline">
+      <DisclosureButton
+        v-if="open"
+        class="mx-3 rounded-2xl bg-yellow-400 px-3 py-2 text-center text-green-950 outline"
+      >
         <b>Skill Summary</b>
       </DisclosureButton>
-      <DisclosureButton v-else class="mx-3 rounded-2xl bg-green-950 py-2 px-3 text-center text-white">
+      <DisclosureButton
+        v-else
+        class="mx-3 rounded-2xl bg-green-950 px-3 py-2 text-center text-white"
+      >
         <b>Skill Summary</b>
       </DisclosureButton>
 
@@ -26,5 +29,13 @@ import SkillSummarySection from "../sections/SkillSummarySection.vue";
         <div class="mx-7"><SkillSummarySection /></div>
       </DisclosurePanel>
     </Disclosure>
+  </div>
+
+  <!-- desktop view -->
+  <div class="my-3 hidden flex-col gap-3 md:flex">
+    <div class="mx-3 flex justify-center"><Avatar /></div>
+    <div class="mx-3"><DownloadCVButton /></div>
+
+    <div class="mx-3"><SkillSummarySection /></div>
   </div>
 </template>

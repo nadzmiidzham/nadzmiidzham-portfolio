1957ea6f97096cf82afbff19b63ca4a974b80c8b
kiogreo@kiogreo-os <nadzmiidzham@gmail.com>
2025-03-22 09:45:26 +0800

    increased the padding for description

--- src/components/TimeLineInfo.vue
+++ src/components/TimeLineInfo.vue
@@ -2,23 +2,23 @@
 const { isStartTime } = defineProps({
   isStartTime: {
     type: Boolean,
-    default: false
-  }
+    default: false,
+  },
 });
 </script>
 
 <template>
   <div class="flex flex-col">
-    <div class="flex flex-row text-white gap-3">
+    <div class="flex flex-row gap-3 text-white">
       <!-- time -->
-      <div class="bg-green-900 px-3 py-2 rounded-xl">
+      <div class="rounded-xl bg-green-900 px-3 py-2">
         <b>
           <slot name="time" class="uppercase"></slot>
         </b>
       </div>
 
       <!-- label -->
-      <div class="bg-green-900 px-3 py-2 rounded-xl">
+      <div class="rounded-xl bg-green-900 px-3 py-2">
         <b>
           <slot name="label" class="uppercase"></slot>
         </b>
@@ -27,16 +27,18 @@ const { isStartTime } = defineProps({
 
     <div class="flex flex-row">
       <!-- Timeline Trail -->
-      <div v-if="!isStartTime" class="mx-5 bg-green-950 w-[2px]"></div>
+      <div v-if="!isStartTime" class="mx-5 w-[2px] bg-green-950"></div>
       <div v-else>
         <!-- starting timeline line trail -->
-        <div class="mx-5 bg-green-950 w-[2px] h-10"></div>
+        <div class="mx-5 h-10 w-[2px] bg-green-950"></div>
 
         <!-- starting timeline circle trail -->
         <div class="mx-3">
-          <div class="relative flex items-center justify-center h-5 w-5">
+          <div class="relative flex h-5 w-5 items-center justify-center">
             <!-- Outer Circle -->
-            <div class="absolute h-5 w-5 rounded-full border-4 border-green-900"></div>
+            <div
+              class="absolute h-5 w-5 rounded-full border-4 border-green-900"
+            ></div>
             <!-- Inner Circle -->
             <div class="absolute h-3 w-3 rounded-full bg-green-600"></div>
           </div>
@@ -44,7 +46,9 @@ const { isStartTime } = defineProps({
       </div>
 
       <!-- Description -->
-      <div class="bg-yellow-200 w-full m-2 p-1 rounded-md outline outline-yellow-600">
+      <div
+        class="m-2 w-full rounded-md bg-yellow-200 p-3 outline outline-yellow-600"
+      >
         <slot name="description"></slot>
       </div>
     </div>
--- src/sections/SkillSection.vue
+++ src/sections/SkillSection.vue
@@ -1,3 +1,7 @@
 <template>
-  <h1>Skill Section</h1>
+  <div class="flex flex-col">
+    <div class="mx-2 rounded-md bg-yellow-200 p-3 outline outline-yellow-600">
+      <h1>Skill section</h1>
+    </div>
+  </div>
 </template>
